e7eefe32c7f4b0843fce37535a61cca92ad5dbf3
striker <armin3600z@gmail.com>
2023-10-26 19:30:09 +0100

    Updated to be truly no collision

--- data.lua
+++ data.lua
@@ -1,11 +1,11 @@
 -- @Author: striker <armin3600z@gmail.com>
 -- @Date: 2023-10-26 12:52:29
 -- @Last Modified by: striker <armin3600z@gmail.com>
--- @Last Modified time: 2023-10-26 12:53:23
+-- @Last Modified time: 2023-10-26 19:29:27
 
 local character = data.raw["character"]["character"]
 
 -- Remove  collision
 if not settings.startup["no-collision-collision"].value then
-  character.collision_box = { { 0, 0 }, { 0, 0 } }
+  character.collision_mask = {}
 end
